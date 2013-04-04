c790dfbf2fe4e0286e148f1b9961bb8796c73126
Max Ott <max.ott@nicta.com.au>
2013-04-04 17:57:55 +1100

    The startup script is now in sbin

--- omf_rc_cloud_node/sbin/run_omf_rc_cloud_node.sh
+++ omf_rc_cloud_node/sbin/run_omf_rc_cloud_node.sh
@@ -1,62 +1,36 @@
 #!/bin/bash
 
-# Startup wrapper for the OMF6 RC
-# detects system-wide RVM installations & Ruby from distro packages
-# and runs OMF6 RC
-
-# system-wide RVM must be installed using
-# '\curl -L https://get.rvm.io | sudo bash -s stable'
+# Startup wrapper for the OMF6 Cloud Node RC
 
 die() { echo "ERROR: $@" 1>&2 ; exit 1; }
 
 RUBY_VER="ruby-1.9.3-p286"
-RUBY_BIN_SUFFIX=""
+
+CONFIG_FILE=/etc/omf_rc/cloud_node.yaml
+CLOUD_NODE_RC_DIR="/usr/local/rvm/gems/${RUBY_VER}@omf/omf_rc_cloud_node"
+
 
 if [ `id -u` != "0" ]; then
 	die "This script is intended to be run as 'root'"
 fi
 
-if [ -e /etc/profile.d/rvm.sh ]; then
-    # use RVM if installed
-    echo "System-wide RVM installation detected"
-    source /etc/profile.d/rvm.sh
-    if [[ $? != 0 ]] ; then
-        die "Failed to initialize RVM environment"
-    fi
-    rvm use $RUBY_VER@omf > /dev/null
-    if [[ $? != 0 ]] ; then
-        die "$RUBY_VER with gemset 'omf' is not installed in your RVM"
-    fi
-    ruby -v | grep 1.9.3  > /dev/null
-    if [[ $? != 0 ]] ; then
-        die "Could not run Ruby 1.9.3"
-    fi
-    gem list | grep omf_rc  > /dev/null
-    if [[ $? != 0 ]] ; then
-        die "The omf_rc gem is not installed in the 'omf' gemset"
-    fi
-else
-    # check for distro ruby when no RVM was found
-    echo "No system-wide RVM installation detected"
-    ruby -v | grep 1.9.3  > /dev/null
-    if [[ $? != 0 ]] ; then
-        ruby1.9.3 -v | grep 1.9.3  > /dev/null
-        if [[ $? != 0 ]] ; then
-            die "Could not run system Ruby 1.9.3. No useable Ruby installation found."
-        fi
-        RUBY_BIN_SUFFIX="1.9.3"
-    fi
-    echo "Ruby 1.9.3 found"
-    gem$RUBY_BIN_SUFFIX list | grep omf_rc  > /dev/null
-    if [[ $? != 0 ]] ; then
-        die "The omf_rc gem is not installed"
-    fi
+if [ ! -e /usr/local/rvm/bin/rvm ]; then
+  die "Can't find 'rvm' installed in '/usr/local/rvm'"
 fi
 
-RC=`which omf_rc`
-if [[ $? != 0 ]] ; then
-    die "could not find omf_rc executable"
+if [ ! -e /usr/local/rvm/gems/${RUBY_VER}@omf ]; then
+  die "Can't find ruby binary '${RUBY_VER}' and 'omf' gem set"
 fi
 
-echo "Running OMF6 RC"
-exec /usr/bin/env ruby$RUBY_BIN_SUFFIX $RC -c /etc/omf_rc/config.yml $@
+if [ ! -e /usr/local/rvm/gems/${RUBY_VER}@omf ]; then
+  die "Can't find ruby binary '${RUBY_VER}' and 'omf' gem set"
+fi
+
+if [ ! -e $CONFIG_FILE ]; then
+  die "Can't find config file '${CONFIG_FILE}'."
+fi
+
+echo "Running OMF6 Cloud Node RC"
+cd $CLOUD_NODE_RC_DIR
+/usr/local/rvm/bin/rvm ${RUBY_VER}@oml exec bundle install
+exec /usr/local/rvm/bin/rvm ${RUBY_VER}@oml exec bundle exec ruby foo -c ${CONFIG_FILE} $@
